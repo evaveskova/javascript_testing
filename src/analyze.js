a0227d15c1a9d78948dad835e35416f301602f20
evaveskova <evaveskova@gmail.com>
2020-01-29 18:49:55 +0100

    add analyze function

---
+++ src/analyze.js
@@ -0,0 +1,20 @@
+const analyze = (arr) => {
+  let obj;
+
+  if (arr === []) {
+    obj = null;
+  } else {
+    const arrAverage = arr.reduce((a, b) => a + b, 0) / arr.length;
+    const arrMin = Math.min.apply(0, arr);
+    const arrMax = Math.max.apply(0, arr);
+    const arrLength = arr.length;
+    const obj = {
+      average: arrAverage,
+      min: arrMin,
+      max: arrMax,
+      length: arrLength
+    }
+  return obj;
+  }
+}
+module.exports = analyze;
--- src/analyze.test.js
+++ src/analyze.test.js
@@ -1,11 +1,11 @@
-// const analyze = require('./analyze');
+const analyze = require('./analyze');
 
 test('return average, min, max, and length of an array', () => {
   const obj = { average: 4, min: 1, max: 8, length: 6 }
-  expect(analyze([1, 8, 3, 4, 2, 6])).toBeCloseTo(obj);
+  expect(analyze([1, 8, 3, 4, 2, 6])).toEqual(obj);
 });
 
 test('returns object of empty array', () => {
   const obj = { average: NaN, min: Infinity, max: -Infinity, length: 0 }
-  expect(analyze([])).toBeCloseTo(obj);
+  expect(analyze([])).toEqual(obj);
 });
